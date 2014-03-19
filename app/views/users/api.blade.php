24931f2dc2734d986254ee211a349244eb6f6399
Rhys Williams <rrrhys@gmail.com>
2014-03-20 05:50:13 +1100

    fix api ref

--- app/views/users/api.blade.php
+++ app/views/users/api.blade.php
@@ -66,7 +66,7 @@ results {
 <h2>Users</h2>
 <code class="collapsed">
 	<h3>Login:</h3>
-	<url>/authenticate (Post)</url>
+	<url>/signin (Post)</url>
 	<arguments>Input fields:
 		<ul>
 			<li>email</li>
@@ -474,7 +474,7 @@ $(function(){
 		validation_reset("login")
 		var email = $("#email").val();
 		var pass = $("#password").val();
-		$.post("/authenticate",
+		$.post("/signin",
 		{
 			email: email,
 			password: pass
