be3caa55b24906a59a89e62208eba74a17391ff5
rrrhys <rrrhys@gmail.com>
2014-06-23 21:51:16 +1000

    dumb view

--- app/views/jobs/partial/table.blade.php
+++ app/views/jobs/partial/table.blade.php
@@ -2,7 +2,7 @@
 	<tr class='clickableRow' href='{{URL::route('job',array($job->id))}}'>
 		<td>{{$job->time_remaining}}</td>
 		<td>{{$job->vehicle_make}} {{$job->vehicle_model}}</td>
-		<td>{{$job->pickup_postcode}} to {{$job->dropoff_postcode}}</td>
+		<td>{{$job->pickup_address_2}} to {{$job->dropoff_address_2}}</td>
 		<td title="View Bids">
 			${{$job->current_bid}}
 			
